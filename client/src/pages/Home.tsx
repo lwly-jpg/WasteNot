cc9fb31d964337810ebf6d217596729942397f49
adon <adnlwly@gmail.com>
2023-01-18 22:25:36 +0000

    Add layout styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
-
   interface Recipe {
     recipe: {
       uri: string;
@@ -9,54 +9,60 @@ const Home = () => {
       images: {
         SMALL: {
           url: string;
-        }
-      }
-    }
+        };
+      };
+    };
   }
 
   const [ingredientsInput, setIngredientsInput] = useState<string>("");
   const [recipeResults, setRecipeResults] = useState<any>();
-  
+
   const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const response = await fetch(`https://wastenot.onrender.com/recipes?ingredients=${ingredientsInput}`)
+    const response = await fetch(
+      `https://wastenot.onrender.com/recipes?ingredients=${ingredientsInput}`
+    );
 
     if (response.status === 200) {
       const data = await response.json();
       setRecipeResults(data.hits);
     }
-
-    
   };
 
-  
   return (
     <>
-      <div className="main">
-        <div className="header">
-          <h1>Random Ingredients? Waste Not...</h1>
-          <h2>
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <h1 className={styles.heading}>Random Ingredients? Waste Not...</h1>
+          <h2 className={styles.subheading}>
             Discover delicious recipes with your random ingredients and waste
             less food.
           </h2>
         </div>
-        <div className="search">
-          <form className="search-bar" onSubmit={handleSearch}>
-            <input className="search-bar-field" type="search" value={ingredientsInput} onChange={(event) => setIngredientsInput(event.target.value)}/>
-            <button className="search-bar-button">Search</button>
+        <div className={styles.search}>
+          <form className={styles.search_bar} onSubmit={handleSearch}>
+            <input
+              className={styles.search_bar_field}
+              type="search"
+              value={ingredientsInput}
+              onChange={(event) => setIngredientsInput(event.target.value)}
+            />
+            <button className={styles.search_bar_button}>Search</button>
           </form>
         </div>
-        <div className="recipie-results">
+        <div className={styles.recipe_results}>
           {recipeResults &&
-          recipeResults.map((result: Recipe) => (
-            <div className="recipie-card" key={result.recipe.uri}>
-              <img src={result.recipe.images.SMALL.url} alt={result.recipe.label}/>
-              {result.recipe.label}
-
-            </div>
-          ))
-          }
+            recipeResults.map((result: Recipe) => (
+              <div className={styles.recipe_card} key={result.recipe.uri}>
+                <img
+                  className={styles.recipe_image}
+                  src={result.recipe.images.SMALL.url}
+                  alt={result.recipe.label}
+                />
+                <h3 className={styles.recipe_label}>{result.recipe.label}</h3>
+              </div>
+            ))}
         </div>
       </div>
     </>
