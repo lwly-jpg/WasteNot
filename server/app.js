f15ca22a4cc89371d115c1362da939af66ae48bd
adon <adnlwly@gmail.com>
2023-01-18 15:36:21 +0000

    Set up Edamam API request

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
+require('dotenv').config();
 
 const port = process.env.PORT;
-
+const appId = process.env.APP_ID;
+const appKey = process.env.APP_KEY;
 
 const app = express();
 
@@ -11,6 +13,20 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Testing if server is up");
+});
+
+app.get("/recipes", async (req, res) => {
+  const ingredients = req.query.ingredients;
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredients}&app_id=${appId}&app_key=${appKey}`
+
+  try {
+    const response = await axios.get(url);
+
+    res.status(200).json(response.data);
+
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
