9eac06a8a75c67a48d9dd4f1131f7f46c3c049f8
adon <adnlwly@gmail.com>
2023-01-19 09:49:11 +0000

    Update layout styles

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -33,36 +33,38 @@ const Home = () => {
   return (
     <>
       <div className={styles.main}>
-        <div className={styles.header}>
-          <h1 className={styles.heading}>Random Ingredients? Waste Not...</h1>
-          <h2 className={styles.subheading}>
-            Discover delicious recipes with your random ingredients and waste
-            less food.
-          </h2>
-        </div>
-        <div className={styles.search}>
-          <form className={styles.search_bar} onSubmit={handleSearch}>
-            <input
-              className={styles.search_bar_field}
-              type="search"
-              value={ingredientsInput}
-              onChange={(event) => setIngredientsInput(event.target.value)}
-            />
-            <button className={styles.search_bar_button}>Search</button>
-          </form>
+        <div className={styles.top_fold}>
+          <div className={styles.header}>
+            <h1 className={styles.heading}>Random Ingredients? Waste Not...</h1>
+            <h2 className={styles.subheading}>
+              Discover delicious recipes with your random ingredients and waste
+              less food.
+            </h2>
+          </div>
+          <div className={styles.search}>
+            <form className={styles.search_bar} onSubmit={handleSearch}>
+              <input
+                className={styles.search_bar_field}
+                type="search"
+                value={ingredientsInput}
+                onChange={(event) => setIngredientsInput(event.target.value)}
+              />
+              <button className={styles.search_bar_button}>Search</button>
+            </form>
+          </div>
         </div>
         <div className={styles.recipe_results}>
-          {recipeResults &&
-            recipeResults.map((result: Recipe) => (
-              <div className={styles.recipe_card} key={result.recipe.uri}>
-                <img
-                  className={styles.recipe_image}
-                  src={result.recipe.images.SMALL.url}
-                  alt={result.recipe.label}
-                />
-                <h3 className={styles.recipe_label}>{result.recipe.label}</h3>
-              </div>
-            ))}
+            {recipeResults &&
+              recipeResults.map((result: Recipe) => (
+                <div className={styles.recipe_card} key={result.recipe.uri}>
+                  <img
+                    className={styles.recipe_image}
+                    src={result.recipe.images.SMALL.url}
+                    alt={result.recipe.label}
+                  />
+                  <h3 className={styles.recipe_label}>{result.recipe.label}</h3>
+                </div>
+              ))}
         </div>
       </div>
     </>
