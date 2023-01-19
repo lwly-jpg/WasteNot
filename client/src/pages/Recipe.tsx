f234cf620dde541a06bd92a008e94ae5856c612a
adon <adnlwly@gmail.com>
2023-01-19 18:38:01 +0000

    Create initial Recipe page layout

--- client/src/pages/Recipe.tsx
+++ client/src/pages/Recipe.tsx
@@ -1,10 +1,17 @@
-import styles from "./Home.module.css";
+import styles from "./Recipe.module.css";
 
 const Recipe = () => {
   return (
-    <>
-      <div>Recipe page</div>
-    </>
+    <div className={styles.recipe_page}>
+      <h1>Recipe page</h1>
+      <div className={styles.main}>
+        <div className={styles.top}>
+          <div className={styles.image}>Image Grid</div>
+          <div className={styles.core_info}>Core Info Grid</div>
+        </div>
+        <div className={styles.bottom}>Bottom Grid</div>
+      </div>
+    </div>
   )
 };
 
