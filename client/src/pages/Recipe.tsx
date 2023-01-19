e931b3347ebd0fc701212e9d64194314f9b49ab1
adon <adnlwly@gmail.com>
2023-01-19 21:05:27 +0000

    Add Method button link

--- client/src/pages/Recipe.tsx
+++ client/src/pages/Recipe.tsx
@@ -4,17 +4,19 @@ import styles from "./Recipe.module.css";
 const Recipe = () => {
   interface Recipe {
     label: string;
-      images: {
-        REGULAR: {
-          url: string;
-        };
+    images: {
+      REGULAR: {
+        url: string;
       };
+    };
     ingredients: [
       ingredient: {
         text: string;
         foodId: string;
       }
     ];
+    url: string;
+    source: string;
   }
 
   const [recipe, setRecipe] = useState<Recipe>();
@@ -22,39 +24,37 @@ const Recipe = () => {
   useEffect(() => {
     fetch(
       `https://wastenot.onrender.com/recipes/b05aeaf36129e3f55ab6242dc0ffb3b4`
-    ).then(
-      response => response.json()
-    ).then(
-      data => setRecipe(data.recipe)
     )
+      .then((response) => response.json())
+      .then((data) => setRecipe(data.recipe));
+  }, []);
 
-  }, [])
-  
   return (
     <div className={styles.recipe_page}>
-      {recipe &&
-      <div className={styles.main}>
-        <div className={styles.top}>
-          <div className={styles.image}>
-          <img
-            className={styles.image}
-            src={recipe.images.REGULAR.url}
-            alt={recipe.label}
-          />
+      {recipe && (
+        <div className={styles.main}>
+          <div className={styles.top}>
+            <img
+              className={styles.image}
+              src={recipe.images.REGULAR.url}
+              alt={recipe.label}
+            />
+            <h1 className={styles.title}>{recipe.label}</h1>
           </div>
-          <div className={styles.core_info}>
-            <h1>{recipe.label}</h1>
+          <div className={styles.ingredients}>
+            <h2 className={styles.ingredients_heading}>Ingredients</h2>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient.foodId}>{ingredient.text}</li>
+            ))}
+          </div>
+          <div className={styles.method}>
+            <h2>Method & More Info</h2>
+            <a href={recipe.url} className={styles.info_button}>Visit {recipe.source}</a>
           </div>
         </div>
-        <div className={styles.bottom}>
-          {recipe.ingredients.map(ingredient => (
-            <li key={ingredient.foodId}>{ingredient.text}</li>
-          ))}
-        </div>
-      </div>
-       }
+      )}
     </div>
-  )
+  );
 };
 
 export default Recipe;
