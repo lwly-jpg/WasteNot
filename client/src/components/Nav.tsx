f08b383d156840223274575cab0fe07df22d8a1f
adon <adnlwly@gmail.com>
2023-01-21 08:49:39 +0000

    Add simple Nav component

---
+++ client/src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import styles from "./Nav.module.css";
+
+const Nav = () => {
+
+  return(
+    <nav className={styles.nav_container}>
+      <h2 className={styles.nav_logo}>WasteNot</h2>
+    </nav>
+  )
+
+};
+
+export default Nav;
