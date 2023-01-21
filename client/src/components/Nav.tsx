31cc4147877133ae6c580705282d2362589c16d6
adon <adnlwly@gmail.com>
2023-01-21 08:51:41 +0000

    Link Nav to Home route

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,10 +1,13 @@
 import styles from "./Nav.module.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
 
   return(
     <nav className={styles.nav_container}>
-      <h2 className={styles.nav_logo}>WasteNot</h2>
+      <Link to="/">
+        <h2 className={styles.nav_logo}>WasteNot</h2>
+      </Link>
     </nav>
   )
 
