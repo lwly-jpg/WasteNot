4d6a76e806bf8a3ce06937870c1dd608b46457fd
adon <adnlwly@gmail.com>
2023-01-22 17:55:51 +0000

    Move createUser helper function into controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -32,15 +32,18 @@ const favouriteRecipe = async (req, res) => {
   const { uri, label, image, user_id } = req.body;
 
   try {
-    const favouriteSQL = "INSERT INTO recipes (uri, label, image, user_id) VALUES ($1, $2, $3, $4) RETURNING id;";
-    const favouritedRecipe = await client.query(
-      favouriteSQL, [uri, label, image, user_id]
-    );
+    const favouriteSQL =
+      "INSERT INTO recipes (uri, label, image, user_id) VALUES ($1, $2, $3, $4) RETURNING id;";
+    const favouritedRecipe = await client.query(favouriteSQL, [
+      uri,
+      label,
+      image,
+      user_id,
+    ]);
 
     const recipe_id = favouritedRecipe.rows[0].id;
 
     res.status(200).json({ label: label, recipe_id: recipe_id });
-
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -53,23 +56,53 @@ const handleLogIn = async (email, password) => {
 
   const logInSQL = "SELECT * FROM users WHERE email = $1;";
 
-  const user = await client.query(logInSQL, [email])
+  const user = await client.query(logInSQL, [email]);
 
   if (user.rowCount === 0) {
-    throw Error('Username or password invalid.');
+    throw Error("Username or password invalid.");
   }
 
   const match = await bcrypt.compare(password, user.rows[0].password);
-  
+
   if (!match) {
-    throw Error('Username or password invalid.');
+    throw Error("Username or password invalid.");
   }
 
   return user;
-}
+};
+
+const createUser = async (email, password) => {
+  if (!email || !password) {
+    throw Error("Both email and password are required.");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email address is invalid.");
+  }
+
+  await checkUserExists(email);
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const createSQL =
+    "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id;";
+  const user = await client.query(createSQL, [email, hashedPassword]);
+
+  return user;
+};
+
+const checkUserExists = async (email) => {
+  const emailSQL = "SELECT * FROM users WHERE email = $1 LIMIT 1;";
+  const emailExists = await client.query(emailSQL, [email]);
+
+  if (emailExists.rowCount !== 0) {
+    throw Error("Email is already in use.");
+  }
+};
 
 module.exports = {
   signUp,
   logIn,
-  favouriteRecipe
+  favouriteRecipe,
 };
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,36 +2,6 @@ const validator = require("validator");
 const bcrypt = require("bcrypt");
 const client = require("../database");
 
-const createUser = async (email, password) => {
-  if (!email || !password) {
-    throw Error("Both email and password are required.");
-  }
 
-  if (!validator.isEmail(email)) {
-    throw Error("Email address is invalid.");
-  }
-
-  await checkUserExists(email);
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const createSQL =
-    "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id;";
-  const user = await client.query(createSQL, [email, hashedPassword]);
-
-  return user;
-};
-
-
-
-const checkUserExists = async (email) => {
-  const emailSQL = "SELECT * FROM users WHERE email = $1 LIMIT 1;";
-  const emailExists = await client.query(emailSQL, [email]);
-
-  if (emailExists.rowCount !== 0) {
-    throw Error("Email is already in use.");
-  }
-}
 
 module.exports = { createUser, logInUser };
